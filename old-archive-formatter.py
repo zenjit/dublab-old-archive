a5538f8d1fa728a750fa1015569c1e17240aa12a
Corrado Scanavino <cscanavino@verbio.com>
2021-04-30 17:12:57 +0200

    fix codification issue + cleaning xml tags within tracklist. json exported

--- old-archive-formatter.py
+++ old-archive-formatter.py
@@ -6,6 +6,7 @@ import re
 import os
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import XMLParser
+import json
 
 def parseXML(xmlfilename, showsitems):
 
@@ -21,8 +22,9 @@ def parseXML(xmlfilename, showsitems):
 
 	# iterate shows items
 	for item in root.findall('./channel/item'):
-		title=item.find('title').text
-		if "attachment" in title:
+		completeTitle=item.find('title').text
+		title,date=getTitleAndDateFromCompleteTitle(completeTitle)
+		if "attachment" in completeTitle:
 			continue
 
 		content=item.find('contentencoded').text
@@ -30,29 +32,41 @@ def parseXML(xmlfilename, showsitems):
 		tracklist=getTracklistFromContent(content)
 
 		# empty news dictionary
-		show={'title': title, 'link':url, 'tracklist':tracklist}
-
+		show={'title': completeTitle.encode("UTF-8"), 'show': title.encode("UTF-8"), 'date': date.encode("UTF-8"), 'link':url.encode("UTF-8"), 'tracklist':tracklist.encode("UTF-8")}
+		#print(tracklist)
 		# append news dictionary to news items list
 		showsitems.append(show)
 
-		print(title + "\n" + url + "\n\n" + tracklist + "\n")
+		print(title.encode("UTF-8") + "\n" + date.encode("UTF-8") + "\n" + url.encode("UTF-8") + "\n\n" + tracklist.encode("UTF-8") + "\n")
 	
 	# return news items list
 	return showsitems
+
+def getTitleAndDateFromCompleteTitle(completeTitle):
+	
+	date=re.search(r'(\d{1,2}\W\d{1,2}\W\d{1,2})', completeTitle)
+	if date:
+		date=date.group(0)
+		title=completeTitle.replace(date,"").strip()
+		date=date.replace(".","/")
+	else:
+		title=completeTitle
+		date=""
+	return title, date
 
 def getUrlFromContent(content):
 
 	url=find_between( content, '<iframe width="100%" height="60" src="', '" frameborder="0" ></iframe>' )
 	if not url:
 		url=find_between( content, '<iframe width="100%" height="60" src="', '" frameborder="0"></iframe>' )
-	urlGood=re.sub(r'%2F', '/', url)
+	url=re.sub(r'%2F', '/', url)
 
-	if "?feed" in urlGood:
-		urlVeryGood=find_between( urlGood, '?feed=', '&hide_cover' )
+	if "?feed" in url:
+		url=find_between( url, '?feed=', '&hide_cover' )
 	else:
-		urlVeryGood=urlGood.replace('widget/iframe/?hide_cover=1&mini=1&light=1&feed=/', '')
-	urlVeryVeryGood=urlVeryGood.replace('https//', '').replace('https%3A//', '')
-	return urlVeryVeryGood
+		url=url.replace('widget/iframe/?hide_cover=1&mini=1&light=1&feed=/', '')
+	url=url.replace('https//', '').replace('https%3A//', '')
+	return url
 
 
 def getTracklistFromContent(content):
@@ -67,6 +81,12 @@ def getTracklistFromContent(content):
 			result=re.search('<p style="white-spacepre-wrap;">(.*)</p></li></ul>', content)
 			if not result:
 				result=re.search('<em>(.*)</em>', content)
+				if result:
+					tracklist=result.group(1)
+				else:
+					result=re.search('>(.*)</p>', content)
+					if result:
+						tracklist=result.group(1)
 			else:
 				tracklist=result.group(1).replace('</p></li><li><p style="white-spacepre-wrap;">','\n')
 				tracklist=tracklist.replace('</p></li></ul><ul data-rte-list="default"><li><p style="white-spacepre-wrap;">', '\n')
@@ -80,24 +100,20 @@ def getTracklistFromContent(content):
 
 
 def cleanTracklist(tracklist):
-	tracklist=tracklist.replace("&amp;","&")
+	tracklist=tracklist.replace("&amp;","&").replace("<br>","").replace("&nbsp;","")
+	tracklist=re.sub("[\<\[].*?[\>\]]", "", tracklist)
 	return tracklist
 
 
 def savetoCSV(newsitems, filename):
-
 	# specifying the fields for csv file
-	fields=['title', 'link', 'tracklist']
-
+	fields=['title', 'completeTitle', 'date' , 'link', 'tracklist']
 	# writing to csv file
 	with open(filename, 'w') as csvfile:
-
 		# creating a csv dict writer object
 		writer=csv.DictWriter(csvfile, fieldnames=fields)
-
 		# writing headers (field names)
 		writer.writeheader()
-
 		# writing data rows
 		writer.writerows(newsitems)
 
@@ -123,15 +139,17 @@ def main():
 	# create empty list for shows items
 	showsitems=[]
 
+	#showsitems=parseXML("prova.xml", showsitems)
+	#return
+
 	for file in os.listdir("./shows/clean"):
 		if file.endswith(".xml"):
-			#print(file)
-			# parse xml file
 			showsitems=parseXML(os.path.join("./shows/clean", file), showsitems)
-			
-	#print showsitems
-	# store news items in a csv file
-	#savetoCSV(newsitems, 'dublab-old-archive.csv')
+
+	print(json.dumps( showsitems, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False))
+	with open('dublab-old-archive.json', 'w') as f:
+		json.dump(showsitems, f, ensure_ascii=False, indent=4)
+	#savetoCSV(showsitems, 'dublab-old-archive.csv')
 	
 if __name__ == "__main__":
 
